8e0df643bd6b0aee83b9441332bbcbfbc13101fa
kenjiru.ro <kenjiru.ro@9f1421ec-5f5e-11de-b31f-81d54249e7d9>
2009-06-22 19:35:42 +0000

    * MainForm.cs: drawing tests

--- trunk/DemoApp/MainForm.cs
+++ trunk/DemoApp/MainForm.cs
@@ -57,13 +57,22 @@ namespace DemoApp
             Pen myPen = new Pen(Color.Tomato, 5);
 			SolidBrush myBrush = new SolidBrush(Color.Aqua);
 			
+			Font drawFont = new Font("Arial", 16);
+    		SolidBrush drawBrush = new SolidBrush(Color.Black);
+/*			
             // draw a line
             g.DrawLine(myPen, 0, 0, 200, 200);
 			// draw an empty circle
 			g.DrawEllipse(myPen, new Rectangle(10, 10, 100, 100));
 			// draw a filled circle
 			g.FillEllipse(myBrush, new Rectangle(10, 130, 100, 100));
-
+*/
+			// draw a triangle
+			g.DrawLines(myPen, new Point[4] { new Point(100, 200), new Point(300, 100), new Point(300, 200), new Point(100, 200) });
+			g.FillEllipse(myBrush, 180, 130, 40, 40);
+			// draw a letter
+			g.DrawString("a", drawFont, drawBrush, new RectangleF(190F, 136F, 30F, 30F));
+			
             myPen.Dispose();
         }
     }
